dadbe2a5fc8878df502c9b9f109657477d6a31b9
Zenn <149383000+Cenx-bash@users.noreply.github.com>
2026-07-26 21:22:49 +0800

    Update and rename aym.cpp to dynamic programming.cpp

---
+++ Fundamentals/dynamic programming.cpp
@@ -0,0 +1,17 @@
+#include  <iostream>
+uisng namespace std;
+
+int main() {
+    int fibonacci(int n) {
+    vector<int> dp(n + 1);
+
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+        dp[i] = dp[i - 1] + dp[i - 2];
+
+    return dp[n];
+}
+
+}
