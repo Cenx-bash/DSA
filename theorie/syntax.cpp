54bcb2d76d715cb417a0790d0d975b213281cd0a
Zenn <149383000+Cenx-bash@users.noreply.github.com>
2026-07-26 21:48:58 +0800

    Update syntax.cpp

--- theorie/syntax.cpp
+++ theorie/syntax.cpp
@@ -1,1 +1,40 @@
-. 
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+
+class DataStructure {
+
+private:
+
+    T data;
+
+public:
+
+    void insert(T value) {
+
+    }
+
+    void remove(T value) {
+
+    }
+
+    bool search(T value) {
+
+        return false;
+
+    }
+
+    void display() {
+
+    }
+
+};
+
+int main() {
+
+    DataStructure<int> structure;
+
+    return 0;
+}
