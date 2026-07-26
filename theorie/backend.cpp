bf74578d84d7b27f51f23faee283b4ee5c952020
Zenn <149383000+Cenx-bash@users.noreply.github.com>
2026-07-26 21:48:16 +0800

    Update backend.cpp

--- theorie/backend.cpp
+++ theorie/backend.cpp
@@ -1,1 +1,59 @@
-. 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Database {
+
+public:
+
+    void connect() {
+
+    }
+
+    void disconnect() {
+
+    }
+
+};
+
+class UserService {
+
+private:
+
+    Database database;
+
+public:
+
+    void login() {
+
+    }
+
+    void registerUser() {
+
+    }
+
+};
+
+class Application {
+
+private:
+
+    UserService service;
+
+public:
+
+    void run() {
+
+    }
+
+};
+
+int main() {
+
+    Application app;
+
+    app.run();
+
+    return 0;
+}
